8da62bdc2f0d4c3be463d1dec9188b974dd69348
Simon Hall <simon.hall@genesis.com.mt>
2023-01-02 15:23:01 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sph-loader-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sph-loader';
+}
